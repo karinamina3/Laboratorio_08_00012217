8ae7d1bce56a4df1dcab195df2bb7198e9dafc66
karinamina3 <00012217@uca.edu.sv>
2018-10-19 09:17:43 -0600

    final

--- practica8/asset/js/app.js
+++ practica8/asset/js/app.js
@@ -25,4 +25,73 @@ const navSlide = ()=>{
 
 navSlide();
 /*********************************Colocar aca el desarrollo de su ejercicio***************************/
+var cont = 1;
+var bitacoras = [];
+var formulario = document.getElementById("bitacora");
+console.log(bitacora); 
 
+formulario.addEventListener("submit", (evt) => {
+    evt.preventDefault(); 
+    let bitacora = { 
+        cant:cont, 
+        fecha: formulario[1].value, 
+        descripcion: formulario[2].value, 
+        cantidad: formulario[3].value 
+    } 
+    bitacoras.push(bitacora);
+    cont++;
+    mostrar();
+}); 
+
+const crearElemento = (bitacora, tbody) =>{ 
+    let tr = document.createElement("tr"); 
+    Object.values(bitacora).forEach(item => { 
+        let td = document.createElement("td"); 
+        let content = document.createTextNode(item); 
+        td.appendChild(content); 
+        tr.setAttribute("class", "click"); 
+        tr.appendChild(td); 
+    }); 
+    tbody.appendChild(tr); 
+} 
+
+const eliminar= (tbody)=>{
+    while (tbody.firstChild) {
+    tbody.removeChild(tbody.firstChild);
+   }
+}
+
+const agregar= ()=>{ 
+    var eventtr = document.querySelectorAll(".click"); 
+    eventtr.forEach((item, index) => { 
+        item.addEventListener("click", () => { 
+            document.querySelector("#fecha").value = item.childNodes[1].textContent; 
+            document.querySelector("#descp").value = item.childNodes[2].textContent; 
+            document.querySelector("#cant").value = item.childNodes[3].textContent; 
+        }); 
+    }) 
+}
+
+const mostrar = ()=>{ 
+    if (document.querySelector(".tabla-btc tbody").childElementCount > 0) { 
+        eliminar(document.querySelector(".tabla-btc tbody")); 
+    } 
+    bitacoras.forEach(item => { 
+        crearElemento(item, document.querySelector(".tabla-btc tbody")); 
+    }); 
+    agregar(); 
+} 
+
+var cajas = document.querySelectorAll(".caja"); 
+console.log(cajas);
+
+cajas.forEach(caja => {
+    caja.oninput = () => { 
+       var valor = caja.value; 
+       if (valor == "" || valor == null) { 
+         caja.style.borderColor = "red";
+       } else {
+         caja.style.borderColor = "#2EFE2E";
+       } 
+    };    
+});
